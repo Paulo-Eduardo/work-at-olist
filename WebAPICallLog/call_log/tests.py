b52394d34952ac8555a5c764907de6fdde76a211
Paulo-Eduardo <paulo.es@live.com>
2018-05-28 18:05:23 -0300

    Creating test for start call endpoint

--- WebAPICallLog/call_log/tests.py
+++ WebAPICallLog/call_log/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+class CallLogTests(TestCase):
+
+    def test_post_call_start(self):
+        data = {
+                    "id":  1,
+                    "type":  "start",
+                    "timestamp":  "",
+                    "call_id":  1,
+                    "source":  "47 996623579",
+                    "destination":  "47 996615673",
+                }
+        response = self.client.get('/calls/', data=data)
+        self.assertEqual(response.status_code, 201)
