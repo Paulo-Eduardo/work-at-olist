e255d0be8f4aa7a456c69ed1fb01e018f6fe4ea9
Paulo-Eduardo <paulo.es@live.com>
2018-05-29 17:43:56 -0300

    Mirror fix to work with heroku

--- WebAPICallLog/WebAPICallLog/settings.py
+++ WebAPICallLog/WebAPICallLog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +129,6 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
