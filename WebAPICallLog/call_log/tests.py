8606e6d63b043b0896328e6483539d721873ca0a
Paulo-Eduardo <paulo.es@live.com>
2018-05-28 22:33:44 -0300

    Refactoring test, changing view
    Now test_post_call_end make sure that the view change duration of the CallRecord

--- WebAPICallLog/call_log/tests.py
+++ WebAPICallLog/call_log/tests.py
@@ -1,19 +1,19 @@
 from django.test import TestCase, RequestFactory
 from call_log.models import Call, CallRecord
-from datetime import datetime
+from datetime import datetime, time
 
 # Create your tests here.
 class CallLogTests(TestCase):
     def setUp(self):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
-        self.user = CallRecord.objects.create(id=1, start_time=datetime.now().strftime('%Y-%m-%d %H:%M'), destination="000", source="000")
+        self.user = CallRecord.objects.create(id=1, start_time="2018-10-10 23:59:00", destination="000", source="000")
 
     def test_can_post_call_start(self):
         data ={ 
                     "id": 1,
                     "typeCall":  "S",
-                    "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M'),
+                    "timestamp": "2018-10-10 21:38:24",
                     "call_id":  1, 
                     "source":  "47 996623579", 
                     "destination":  "47 996615673", 
@@ -34,7 +34,7 @@ class CallLogTests(TestCase):
         data = {
                     "id":  2,
                     "typeCall": "E",
-                    "timestamp":  datetime.now().strftime('%Y-%m-%d %H:%M'),
+                    "timestamp":  "2018-10-11 00:01:00",
                     "call_id":  1,
                 }
         response = self.client.post('/calls/', data)
@@ -45,4 +45,4 @@ class CallLogTests(TestCase):
         cr = CallRecord.objects.all()[0]
         assert cr.id, 1
         assert cr.end_time, data["timestamp"]
-    
+        self.assertEqual(cr.duration, time(0,2,0))    
--- WebAPICallLog/call_log/views.py
+++ WebAPICallLog/call_log/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from call_log.serializers import CallSerializer, CallRecordSerializer
 from call_log.models import CallRecord
+from datetime import datetime, time
 
 # Create your views here.
 class CallList(APIView):
@@ -33,8 +34,13 @@ class CallList(APIView):
 
     def save_data_record_end(self, data):
         call_record = self.get_object(data["call_id"])
+        start_date = datetime.strptime(str(call_record.start_time)[:19], "%Y-%m-%d %H:%M:%S")
+        end_date = datetime.strptime(data["timestamp"], "%Y-%m-%d %H:%M:%S")
+        dif_dates = end_date - start_date
+        duration = time(dif_dates.seconds//3600, (dif_dates.seconds//60)%60, dif_dates.seconds%60)        
         call_record_end = {
             "end_time" : data["timestamp"],
+            "duration" : duration,
         }
 
         serializer = CallRecordSerializer(call_record, call_record_end)
